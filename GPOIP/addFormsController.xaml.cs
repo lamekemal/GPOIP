0663bad88eab7a99e336064fe9ad1aff9c19c51e
Lamekemal <handill@live.fr>
2022-02-25 00:37:44 +0100

    Ajoutez des fichiers projet.

---
+++ GPOIP/addFormsController.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GPOIP
+{
+    /// <summary>
+    /// Logique d'interaction pour addFormsController.xaml
+    /// </summary>
+    public partial class addFormsController : Page
+    {
+        public addFormsController()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GPOIP/login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GPOIP
+{
+    /// <summary>
+    /// Logique d'interaction pour login.xaml
+    /// </summary>
+    public partial class login : Window
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainPage = new MainWindow();
+            mainPage.Show();
+            Close();
+        }
+    }
+}
